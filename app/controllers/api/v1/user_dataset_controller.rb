e223cec782eec27199d7013497f992e4ca28ed27
SuperJesseH <therealjhorwitz@gmail.com>
2018-06-27 10:33:10 -0400

    fixed up routes

--- app/controllers/api/v1/user_dataset_controller.rb
+++ app/controllers/api/v1/user_dataset_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::UserDatasetController < ApplicationController
+
   def index
+    #grab all users
+    @users = User.all
+    #send all users to client in JSON format
+    render json: @users
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'user_dataset/index'
-      get 'user_dataset/show'
-      get 'user_dataset/create'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'dataset/index'
-      get 'dataset/show'
-      get 'dataset/create'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'user/index'
-      get 'user/show'
-      get 'user/create'
+      resources :users, only: [:index, :show, :create]
+      resources :datasets, only: [:index, :show, :create]
+      resources :user_datasets, only: [:index, :show, :create]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
