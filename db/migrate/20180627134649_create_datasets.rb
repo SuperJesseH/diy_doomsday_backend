7812473ad8f5271b39d9bea101960a431a8d12c5
SuperJesseH <therealjhorwitz@gmail.com>
2018-06-27 10:56:20 -0400

    added some seed data

--- db/migrate/20180627134649_create_datasets.rb
+++ db/migrate/20180627134649_create_datasets.rb
@@ -1,6 +1,7 @@
 class CreateDatasets < ActiveRecord::Migration[5.2]
   def change
     create_table :datasets do |t|
+      t.string :name
       t.string :src
       t.string :desc
       t.string :normalizer
