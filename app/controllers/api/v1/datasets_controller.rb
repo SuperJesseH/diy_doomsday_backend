328483d103da29afe660600543b95f7af7f21192
SuperJesseH <therealjhorwitz@gmail.com>
2018-07-02 10:40:21 -0400

    allow for requests to database index

--- app/controllers/api/v1/datasets_controller.rb
+++ app/controllers/api/v1/datasets_controller.rb
@@ -1,10 +1,19 @@
-class Api::V1::DatasetController < ApplicationController
+class Api::V1::DatasetsController < ApplicationController
+
+
   def index
+    #grab all avalible datasets
+    @data = Dataset.all
+    #send all datasets to client in JSON format
+    render json: @data
   end
 
   def show
+
   end
 
   def create
   end
+
+
 end
