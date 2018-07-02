168f0b5201c02dcae1cf18a3444c49a4683ecbc9
SuperJesseH <therealjhorwitz@gmail.com>
2018-07-02 16:27:41 -0400

    added controller action to update or create User_dataset weights

--- app/controllers/api/v1/user_datasets_controller.rb
+++ app/controllers/api/v1/user_datasets_controller.rb
@@ -10,11 +10,19 @@ class Api::V1::UserDatasetsController < ApplicationController
   def show
     # shows specific users data and weights
     # api/v1/user_datasets/{user_id}
-    @UserSet = UserDataset.select{ |item| item[:user_id] == params["id"].to_i}
-    render json: @UserSet
+    @userSet = UserDataset.select{ |item| item[:user_id] == params["id"].to_i}
+    render json: @userSet
   end
 
   def create
+    @userSet = UserDataset.where(:user_id => params["user_id"], :dataset_id => params["dataset_id"]).first_or_create
+
+    @userSet.weight = params["weight"]
+
+    @userSet.save
+    
+    puts @userSet.weight
+    render json: @userSet
   end
 
   def update
