823f7436b2555c67023428f0cec66e627e75709e
SuperJesseH <therealjhorwitz@gmail.com>
2018-07-11 16:39:01 -0400

    serializer bug

--- app/models/serializers/user_serializer.rb
+++ app/models/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
-class UserSerializer < ActiveModel::UserSerializer
+class UserSerializer < ActiveModel::Serializer
   attributes :name, :id
   has_many :user_datasets
   has_many :datasets, through: :user_datasets
